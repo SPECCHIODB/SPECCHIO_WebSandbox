8452c1c08e0a63a6f78b9fe55a505861d99f6b7e
Daniel Kükenbrink <Daniel Kkenbrink@4fa72ff2-75d9-4fc5-a188-d8e038bdffb6>
2013-03-20 17:03:24 +0000

    Changes for debugging the navigation tree
    git-svn-id: svn+ssh://scm.geo.uzh.ch/projects/specchiowebsandbox/specchiowebsandbox_svn@26 4fa72ff2-75d9-4fc5-a188-d8e038bdffb6

--- SpecchioWebSandbox/src/com/example/specchiowebsandbox/data/SpectrumData.java
+++ SpecchioWebSandbox/src/com/example/specchiowebsandbox/data/SpectrumData.java
@@ -7,6 +7,9 @@ public class SpectrumData implements Serializable{
 	private String filename;
 	private int spectrum_id;
 	private int measurement_unit;
+	private long campaign_id;
+	private String campaign_name;
+	private Object parent;
 	
 	public String getFilename() {
 		return filename;
@@ -26,6 +29,24 @@ public class SpectrumData implements Serializable{
 	}
 	public void setMeasurement_unit(int measurement_unit) {
 		this.measurement_unit = measurement_unit;
+	}
+	public long getCampaignId() {
+		return campaign_id;
+	}
+	public void setCampaignId(long campaign_id) {
+		this.campaign_id = campaign_id;
+	}
+	public String getCampaignName() {
+		return campaign_name;
+	}
+	public void setCampaignName(String campaign_name) {
+		this.campaign_name = campaign_name;
+	}
+	public Object getParent() {
+		return parent;
+	}
+	public void setParent(Object parent) {
+		this.parent = parent;
 	}
 	@Override
     public String toString() {
