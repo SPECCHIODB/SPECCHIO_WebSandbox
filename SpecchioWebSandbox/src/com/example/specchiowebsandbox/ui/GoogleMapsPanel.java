40f70fba4140d369a9f8e7c32f90f4cefe8a5379
Daniel Kükenbrink <Daniel Kkenbrink@4fa72ff2-75d9-4fc5-a188-d8e038bdffb6>
2013-04-10 15:29:21 +0000

    Use of GoogleMapsWidget and finally solved problem with marker offset
    git-svn-id: svn+ssh://scm.geo.uzh.ch/projects/specchiowebsandbox/specchiowebsandbox_svn@45 4fa72ff2-75d9-4fc5-a188-d8e038bdffb6

--- SpecchioWebSandbox/src/com/example/specchiowebsandbox/ui/GoogleMapsPanel.java
+++ SpecchioWebSandbox/src/com/example/specchiowebsandbox/ui/GoogleMapsPanel.java
@@ -2,6 +2,9 @@ package com.example.specchiowebsandbox.ui;
 
 import java.awt.geom.Point2D;
 
+import org.vaadin.hezamu.googlemapwidget.GoogleMap;
+import org.vaadin.hezamu.googlemapwidget.GoogleMap.MapControl;
+import org.vaadin.hezamu.googlemapwidget.overlay.BasicMarker;
 import org.vaadin.vol.GoogleStreetMapLayer;
 import org.vaadin.vol.Layer;
 import org.vaadin.vol.Marker;
@@ -70,73 +73,55 @@ public class GoogleMapsPanel extends VerticalLayout{
 		
 		
 		
-		final OpenLayersMap map = new OpenLayersMap();
-//		map.setApiProjection("WGS84");
-		map.setApiProjection("EPSG:4326");
-		
-		GoogleStreetMapLayer google_map = new GoogleStreetMapLayer();
-		
-		google_map.setParent(map);
-		
-//		google_map.setProjection("EPSG:4326");
-		
-//		OpenStreetMapLayer os_map = new OpenStreetMapLayer();
-//		os_map.setProjection("EPSG:4326");
-		
-		map.addLayer(google_map);
-		
-		MarkerLayer markers = new MarkerLayer();
-		
-		map.addLayer(markers);
-		
-	
-		
-	
-		
-		VectorLayer vectors = new VectorLayer();
-		map.addLayer(vectors);
-		
-//		Projection proj = Projection.get("EPSG:4326");
-		
-//		LonLat lonlat = LonLat.create(meta.longitude, meta.latitude);
-//		lonlat.transform(proj, Projection.get(map.getApiProjection()));
-		
-		
-		
-		
-		
-		
-		
-		Point point = new Point(meta.longitude, meta.latitude);
-		PointVector pvec = new PointVector();
-		pvec.setProjection("EPSG:4326");
-		pvec.setPoints(point);
-
-		vectors.addVector(pvec);
-		
-
-		
-		Marker test = new Marker(meta.longitude, meta.latitude);
-		
-		
-
-		
-		markers.addMarker(test);
-		
-		
-		
+//		final OpenLayersMap map = new OpenLayersMap();
+//		map.setApiProjection("EPSG:4326");
+//		
+//		GoogleStreetMapLayer google_map = new GoogleStreetMapLayer();
+//		
+//		google_map.setParent(map);
+//		
 //		map.addLayer(google_map);
-//		map.addLayer(os_map);
+//		
+//		MarkerLayer markers = new MarkerLayer();
+//		
+//		map.addLayer(markers);
+//		
+//	
+//		
+//	
+//		
+//		VectorLayer vectors = new VectorLayer();
+//		map.addLayer(vectors);
+//		
+//		Point point = new Point(meta.longitude, meta.latitude);
+//		PointVector pvec = new PointVector();
+//		pvec.setProjection("EPSG:4326");
+//		pvec.setPoints(point);
+//
+//		vectors.addVector(pvec);
+//		
+//
+//		
+//		Marker test = new Marker(meta.longitude, meta.latitude);
+//		
+//		
+//
+//		
+//		markers.addMarker(test);
+//		
+//		
+//		map.setCenter(meta.longitude, meta.latitude);
+//		
+//		map.setZoom(3);
+//		
+//		map.setHeight(240, UNITS_PIXELS);
+//		map.setWidth("100%");
 		
-//		map.addLayer((Layer) test);
-		
-		
-		map.setCenter(meta.longitude, meta.latitude);
-		
-		map.setZoom(3);
-		
-		map.setHeight(240, UNITS_PIXELS);
+		GoogleMap map = new GoogleMap(app, new Point2D.Double(-1*meta.longitude,meta.latitude), 2);
 		map.setWidth("100%");
+		map.setHeight("250px");
+		
+		map.addMarker(new BasicMarker(1L,new Point2D.Double(-1*meta.longitude,meta.latitude),"Test Marker"));
 		
 		grid.addComponent(map);
 		
